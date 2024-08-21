5fc390b9b0b851017fa7417012d1cc9a8744c213
Joyce Njoroge <136052595+JoyceNjoro1@users.noreply.github.com>
2024-08-21 19:16:59 +0300

    nodejs

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 function displayMessage(message) {
-    process.stdout.write(message + '\n'); // Add a newline for better formatting
-  }
-  
-  module.exports = displayMessage;
+    console.log(message);
+}
+
+module.exports = displayMessage;
